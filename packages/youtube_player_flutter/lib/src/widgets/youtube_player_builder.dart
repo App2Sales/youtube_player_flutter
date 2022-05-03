3402b6b3f06276c92ad447e067bbfa093b5a08ea
Gabriel Damasceno <bieldamasceno@gmail.com>
2022-05-03 15:53:44 -0300

    feat: Add safearea to youtube player builder

--- packages/youtube_player_flutter/lib/src/widgets/youtube_player_builder.dart
+++ packages/youtube_player_flutter/lib/src/widgets/youtube_player_builder.dart
@@ -64,18 +64,20 @@ class _YoutubePlayerBuilderState extends State<YoutubePlayerBuilder>
 
   @override
   Widget build(BuildContext context) {
-    final _player = Container(
-      key: playerKey,
-      child: WillPopScope(
-        onWillPop: () async {
-          final controller = widget.player.controller;
-          if (controller.value.isFullScreen) {
-            widget.player.controller.toggleFullScreenMode();
-            return false;
-          }
-          return true;
-        },
-        child: widget.player,
+    final _player = SafeArea(
+      child: Container(
+        key: playerKey,
+        child: WillPopScope(
+          onWillPop: () async {
+            final controller = widget.player.controller;
+            if (controller.value.isFullScreen) {
+              widget.player.controller.toggleFullScreenMode();
+              return false;
+            }
+            return true;
+          },
+          child: widget.player,
+        ),
       ),
     );
     final child = widget.builder(context, _player);
